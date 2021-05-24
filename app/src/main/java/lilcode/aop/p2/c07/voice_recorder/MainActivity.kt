c4abd678530119235e47fbd8af61a24dcabd858d
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-24 16:00:16 +0900

    Feat: 초기 버튼 상태 설정

--- app/src/main/java/lilcode/aop/p2/c07/voice_recorder/MainActivity.kt
+++ app/src/main/java/lilcode/aop/p2/c07/voice_recorder/MainActivity.kt
@@ -4,8 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val recordButton: RecordButton by lazy {
+        findViewById(R.id.recordButton)
+    }
+
+    private var state = State.BEFORE_RECORDING
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews(){
+        recordButton.updateIconWithState(state)
     }
 }
