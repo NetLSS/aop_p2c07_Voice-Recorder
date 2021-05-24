fccce71663b52fb2d6e126e0d0a0233a97825858
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-24 16:20:57 +0900

    Feat: 음성 녹음 권한 runTime permission 얻기

--- app/src/main/java/lilcode/aop/p2/c07/voice_recorder/MainActivity.kt
+++ app/src/main/java/lilcode/aop/p2/c07/voice_recorder/MainActivity.kt
@@ -1,5 +1,6 @@
 package lilcode.aop.p2.c07.voice_recorder
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,16 +10,44 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.recordButton)
     }
 
+    private val requiredPermissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
     private var state = State.BEFORE_RECORDING
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestAudioPermission()
         initViews()
     }
 
-    private fun initViews(){
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // 요청한 권한에 대한 결과
+
+        val audioRecordPermissionGranted =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                    grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if (!audioRecordPermissionGranted){
+            finish() // 거절 할 경우 앱 종료
+        }
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    private fun initViews() {
         recordButton.updateIconWithState(state)
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 }
